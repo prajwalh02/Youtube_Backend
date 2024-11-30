190817d37b013401627a9506189e9ddc26ad394e
Prajwal Hole <prajwal.hole.28.2@mountblue.tech>
2024-11-30 14:20:04 +0530

    Custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-    path: './.env',
-})
+  path: "./.env",
+});
 
 
-connectDB()
+const startServer = async() => {
+  try {
+    await connectDB();
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
 
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+
+  } catch (error) {
+    console.log("MONGO DB connection failed !!!", error);
+  } 
+}
+
+startServer();
+
+
+// connectDB()
+//   .then(() => {
+//     app.on("error", (error) => {
+//       console.log("ERROR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`Server is running at port: ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("MONGO DB connection failed !!!", err);
+//   });
 
